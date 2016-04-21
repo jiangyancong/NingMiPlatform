48ea69fe93074803f39dbf80693f41f41a1922a3
StanleyJiang <jiangyancong@gmail.com>
2016-04-21 16:33:46 +0800

    create pay success view

---
+++ testGitHub/testGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGitHub
+//
+//  Created by 江彦聪 on 16/4/21.
+//  Copyright © 2016年 NingMi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
